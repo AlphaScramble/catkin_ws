eccfdf16ebc5304a1528c69eeea210b5bc16b69d
AlphaScramble <64156718+AlphaScramble@users.noreply.github.com>
2023-02-18 17:34:46 +0300

    Add files via upload

---
+++ build/opencv_uygulama/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/omer/warehouse-drone/computer_vision/catkin_ws/src/opencv_uygulama
+# Build directory: /home/omer/warehouse-drone/computer_vision/catkin_ws/build/opencv_uygulama
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
